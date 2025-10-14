f6f53ed56d779b1e5367f14dab4ca3391887a29c
Graham Kelly <gkgoat6700@gmail.com>
2025-10-14 11:16:21 -0400

    fixes

--- crates/rage-compiler/src/source.rs
+++ crates/rage-compiler/src/source.rs
@@ -1,11 +1,12 @@
-use std::rc::Rc;
+use std::{path::PathBuf, rc::Rc};
 
-use swc_common::{source_map::RealFileLoader, sync::Lrc, FileLoader, FileName};
+use swc_common::{FileLoader, FileName, source_map::RealFileLoader, sync::Lrc};
 
 use crate::*;
 
 pub(crate) struct RustLoader {
     pub(crate) map: OnceMap<[u8; 32], Box<swc_common::sync::Lrc<SourceFile>>>,
+    pub(crate) files: OnceMap<[u8; 32], Box<PathBuf>>,
     pub(crate) sm: swc_common::sync::Lrc<SourceMap>,
     pub(crate) loader: swc_common::sync::Lrc<dyn FileLoader>,
 }
@@ -14,6 +15,7 @@ impl Default for RustLoader {
         Self {
             map: Default::default(),
             sm: Default::default(),
+            files: Default::default(),
             loader: Lrc::new(RealFileLoader {}),
         }
     }
@@ -27,7 +29,10 @@ impl RustLoader {
                 |_| {
                     self.loader
                         .read_file(a)
-                        .map(|b| self.sm.new_source_file(Lrc::new(FileName::Real(a.to_path_buf())), b))
+                        .map(|b| {
+                            self.sm
+                                .new_source_file(Lrc::new(FileName::Real(a.to_path_buf())), b)
+                        })
                         .map(Box::new)
                 },
             )?
@@ -35,7 +40,10 @@ impl RustLoader {
     }
     pub(crate) fn map_span(&self, s: &proc_macro2::Span) -> anyhow::Result<swc_common::Span> {
         let b = s.byte_range();
-        let f = s.local_file().context("in getting the file")?;
+        let f = self
+            .files
+            .get::<[u8; 32]>(&sha3::Sha3_256::digest(s.file()).into())
+            .context("in getting the mapped file")?;
         let f = self.load(&f).context("in loading the file")?;
         let lo = f.start_pos.0 + (b.start as u32);
         let hi = f.start_pos.0 + (b.end as u32);
@@ -44,9 +52,16 @@ impl RustLoader {
             hi: swc_common::BytePos(hi),
         })
     }
-    pub(crate) fn syn<T: syn::parse::Parse>(&self, a: &Path) -> anyhow::Result<T> {
+    pub(crate) fn syn<T: syn::parse::Parse + syn::spanned::Spanned>(
+        &self,
+        a: &Path,
+    ) -> anyhow::Result<T> {
         let l = self.load(a)?;
         let s: T = syn::parse_str(&l.src)?;
+        self.files.insert(
+            sha3::Sha3_256::digest(syn::spanned::Spanned::span(&s).file()).into(),
+            |_| Box::new(a.to_owned()),
+        );
         Ok(s)
     }
 }
